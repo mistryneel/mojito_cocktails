249a45875ac4070ab69a850a6ff737c9a18bd39a
Neel Mistry <mistrynpk@gmail.com>
2025-07-15 17:06:32 -0500

    hero section details

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl text-indigo-300">Welcome to My React App</h1>
-      <p>This is a simple React application.</p>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
   );
 };
 
